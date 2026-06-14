584c3edfa90da3065bd7980488f8ed2fd402814a
ashraf <ashraf1abdelrasool@gmail.com>
2026-06-14 21:02:59 +0300

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.py
@@ -0,0 +1,37 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+
+        left_map={}
+        right_map={}
+        for i in range(len(nums)):
+            inverted_i=len(nums)-i-1
+            if i==0:
+                left_map[i]=1
+                right_map[inverted_i]=1
+            else:
+                left_map[i]=nums[i-1]*left_map[i-1]
+                right_map[inverted_i]=nums[inverted_i+1]*right_map[inverted_i+1]
+
+
+
+        def calc_left(i):
+            result=1
+
+            for i in range(0,i):
+                result *=nums[i]
+            return result    
+
+        def calc_right(i):
+            result=1
+
+            for i in range(i+1,len(nums)):
+                result *=nums[i]
+            return result    
+
+
+        result=[]
+        for i in range(len(nums)):
+
+            result.append(left_map[i] * right_map[i])
+        return result
